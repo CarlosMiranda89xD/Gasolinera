bda1b02389c7baa40a1f74dda0ff8111dbe22fa1
Robertor32 <132176798+Robertor32@users.noreply.github.com>
2024-06-05 15:27:03 -0600

    ValidacionResultado.cs

---
+++ Gasolinera/ValidacionResultado.cs
@@ -0,0 +1,8 @@
+public class ValidacionResultado
+{
+    public bool EsValido { get; set; }
+    public int IdBomba { get; set; }
+    public string TipoAbastecimiento { get; set; }
+    public string NombreCliente { get; set; }
+    public double MontoPrepago { get; set; }
+}
